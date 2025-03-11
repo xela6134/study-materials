7148dafc2f1b2bc82d7fec5404b32d4a7b4f6c11
xela6134 <leol3o05@gmail.com>
2025-03-12 10:36:31 +1100

    Changed file permissions

---
+++ COMP2041/week4/mine/hi.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int hi() {
+    int var1 = 2;
+}
+
+int main() {
+    int var1 = 3;
+    hi();
+}
--- COMP2041/week4/tut/q4.sh
+++ COMP2041/week4/tut/q4.sh
@@ -9,6 +9,9 @@ do
 
     temporary_file="$(mktemp)"
 
-    sed -E 's/COMP2041/COMP2042/g; s/COMP9044/COMP9042/g' "$file" > "$temporary_file" && mv "$temporary_file" "$file"
+    # &&: running multiple commands at the same time (if they're all valid with no errors)
+    sed -E 's/COMP2041/COMP2042/g; s/COMP9044/COMP9042/g' "$file" > "$temporary_file" 
+    mv "$temporary_file" "$file"
+
     rm -f "$temporary_file"
 done
